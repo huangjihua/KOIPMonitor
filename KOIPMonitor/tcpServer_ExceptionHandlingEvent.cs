f806f72bef084097de81c52276d20d33895e98ec
huangjihua <steven-online@hotmail.com>
2014-12-11 10:23:42 +0800

    KOIPMonitorProject

--- KOIPMonitor/tcpServer_ExceptionHandlingEvent.cs
+++ KOIPMonitor/tcpServer_ExceptionHandlingEvent.cs
@@ -11,7 +11,7 @@ namespace KOIPMonitor
         public static void ReceiveEvent(StateObject requset)
         {
             try
-            {
+            { 
 
                 if (requset == null)
                 {
